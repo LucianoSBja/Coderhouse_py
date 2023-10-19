339dd4f881f4cd05a5bed17dbae4817c2af4c527
LucianoSBja <lucianosotobonja95@gmail.com>
2023-10-19 02:12:25 -0300

    Segunda pre-entrega

--- seg_entrega/setup.py
+++ seg_entrega/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="seg_entregaSotoBonja",
+    name="segunda_pre_entregaSotoBonja",
     version="0.1",
+    description="Primer paquete distribuido",
+    author="Clase CODER - Python",
+    author_email="lucianosotobonja95@gmail.com",
+
     packages=["seg_entregaSotoBonja"],
-    install_requires=[],
 )
