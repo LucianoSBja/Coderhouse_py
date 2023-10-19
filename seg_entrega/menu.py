51ba3fedf452a2353e5808cf332b7d7cd42a4c2d
LucianoSBja <lucianosotobonja95@gmail.com>
2023-10-19 01:55:47 -0300

    Segunda pre-entrega

---
+++ seg_entrega/menu.py
@@ -0,0 +1,7 @@
+from seg_entregaSotoBonja.client import Client
+
+cliente1 = Client("Juan", "Pérez", 23, "juan.perez@example.com", "Calle 123, Ciudad de México", "555-555-5555")
+print(cliente1)
+
+cliente2 = Client("María", "García", 18, "maria.garcia@example.com", "Avenida 456, Guadalajara", "333-333-3333")
+print(cliente2)
---
+++ seg_entrega/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="seg_entregaSotoBonja",
+    version="0.1",
+    packages=["seg_entregaSotoBonja"],
+    install_requires=[],
+)
